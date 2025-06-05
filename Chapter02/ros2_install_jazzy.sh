cef6673b3f7602b18345e0ebcf455e5e5a651d0e
runtimerobotics <runtimerobotics@gmail.com>
2025-06-06 02:31:29 +0530

    Updated Chapter02 code

--- Chapter02/ros2_install_jazzy.sh
+++ Chapter02/ros2_install_jazzy.sh
@@ -86,27 +86,12 @@ echo ">>> {Installing ROS 2 APT Source package}"
 
 
 sudo apt update 
-sudo apt install -y curl openssl ros2-apt-source
+sudo apt install -y curl openssl 
 
-#fi
 
-echo "#######################################################################################################################"
-echo ""
-#Adding keys
-echo ">>> {Waiting for adding keys, it will take few seconds}"
-echo ""
-#sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-
-#Checking return value is OK
-#case $ret in
-#  "OK" )
-#  ;;
-#  *)
-#    echo ">>> {ERROR: Unable to add ROS Jazzy keys}"
-#    exit 0
-#esac
-
-#echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb" # If using Ubuntu derivates use $UBUNTU_CODENAME
+sudo apt install /tmp/ros2-apt-source.deb
 
 
 echo ">>> {Done: Added Keys}"
